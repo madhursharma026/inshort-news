86f2971614bad8fa841125a67e06c5185c7b859a
Madhur Sharma <105696243+madhursharma026@users.noreply.github.com>
2024-09-08 12:05:57 +0530

    Initial Commit

--- app/_layout.js
+++ app/_layout.js
@@ -1,38 +1,19 @@
 import tw from "twrnc";
 import { Stack } from "expo-router";
 import Constants from "expo-constants";
-import React, { useEffect, useState } from "react";
 import { View, StatusBar, LogBox } from "react-native";
 import { LanguageProvider } from "../context/LanguageContext";
 import { BookmarkProvider } from "../context/BookmarkContext";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { ThemeProvider, useTheme } from "../context/ThemeContext";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Layout = () => {
-  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
-
-  useEffect(() => {
-    const checkFirstLaunch = async () => {
-      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
-      setIsFirstLaunch(hasLaunched === null);
-    };
-
-    checkFirstLaunch();
-  }, []);
-
-  useEffect(() => {
-    LogBox.ignoreAllLogs(); // Ignore all log notifications
-  }, []);
-
-  if (isFirstLaunch === null) return null;
-
   return (
     <SafeAreaProvider style={tw`flex-1`}>
       <ThemeProvider>
         <LanguageProvider>
           <BookmarkProvider>
-            <ThemedLayout isFirstLaunch={isFirstLaunch} />
+            <ThemedLayout />
           </BookmarkProvider>
         </LanguageProvider>
       </ThemeProvider>
@@ -40,7 +21,7 @@ const Layout = () => {
   );
 };
 
-function ThemedLayout({ isFirstLaunch }) {
+function ThemedLayout() {
   const { isDarkMode } = useTheme();
   const backgroundColor = isDarkMode ? "#282C35" : "#f1f1f1";
   const barStyle = isDarkMode ? "light-content" : "dark-content";
@@ -54,23 +35,18 @@ function ThemedLayout({ isFirstLaunch }) {
           { backgroundColor },
         ]}
       >
-        {isFirstLaunch ? (
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="onboarding/screen1" />
-            <Stack.Screen name="onboarding/screen2" />
-            <Stack.Screen name="onboarding/screen3" />
-          </Stack>
-        ) : (
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="(tabs)" />
-            <Stack.Screen name="BookmarkNews/index" />
-            <Stack.Screen name="ContactUs/index" />
-            <Stack.Screen name="TermsOfService/index" />
-            <Stack.Screen name="LinkViewer/index" />
-            <Stack.Screen name="ImageViewer/index" />
-            <Stack.Screen name="SingleArticle/index" />
-          </Stack>
-        )}
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="onboarding/screen1" />
+          <Stack.Screen name="onboarding/screen2" />
+          <Stack.Screen name="onboarding/screen3" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen name="BookmarkNews/index" />
+          <Stack.Screen name="ContactUs/index" />
+          <Stack.Screen name="TermsOfService/index" />
+          <Stack.Screen name="LinkViewer/index" />
+          <Stack.Screen name="ImageViewer/index" />
+          <Stack.Screen name="SingleArticle/index" />
+        </Stack>
       </View>
     </>
   );
--- app/index.js
+++ app/index.js
@@ -1,7 +1,33 @@
-// app/index.js
-import React from "react";
+import { LogBox } from "react-native";
 import { Redirect } from "expo-router";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
-  return <Redirect href="/onboarding" />;
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    const checkFirstLaunch = async () => {
+      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
+      setIsFirstLaunch(hasLaunched === null);
+    };
+
+    checkFirstLaunch();
+  }, []);
+
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+  }, []);
+
+  if (isFirstLaunch === null) return null;
+
+  return (
+    <>
+      {isFirstLaunch ? (
+        <Redirect href="/onboarding" />
+      ) : (
+        <Redirect href="/(tabs)" />
+      )}
+    </>
+  );
 }
